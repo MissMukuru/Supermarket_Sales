6fb045ed94bf95c2bf3e16da3a37b01a1a2bf540
MissMukuru <a.mukuruzawadi@gmail.com>
2025-10-06 00:35:37 +0300

    The prediction file

--- Supermarket_sales/modeling/predict.py
+++ Supermarket_sales/modeling/predict.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
-from loguru import logger
-from tqdm import tqdm
+import joblib
+import pandas as pd
 import typer
+from loguru import logger
 
 from Supermarket_sales.config import MODELS_DIR, PROCESSED_DATA_DIR
 
@@ -11,37 +12,31 @@ app = typer.Typer()
 
 @app.command()
 def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
     features_path: Path = PROCESSED_DATA_DIR / "test_features.csv",
     regression_model_path: Path = MODELS_DIR / "Random_forest_regression_model.pkl",
     classification_model_path: Path = MODELS_DIR / "Random_forest_classifier_model.pkl",
     predictions_path: Path = PROCESSED_DATA_DIR / "test_predictions.csv",
-    # -----------------------------------------
 ):
     logger.info(f'loading features from {features_path}')
     df = pd.read_csv(features_path)
 
-    X=df.drop(columns=['Target_Total','HighSpender'],errors='ignore')
-
-    logger.info(f'Loading features from {features_path}')
-    df=pd.read_csv(features_path)
+    X = df.drop(columns=['Target_Total', 'HighSpender'], errors='ignore')
 
     logger.info('Loading the models')
-    reg_models=joblib.load(regression_model_path)
-    clf_model=joblib(classification_model_path)
+    reg_model = joblib.load(regression_model_path)
+    clf_model = joblib.load(classification_model_path)
 
     logger.info("Making predictions...")
-    y_reg_pred=regression_model_path(X)
-    y_clf_pred=classification_model_path(X)
+    y_reg_pred = reg_model.predict(X)
+    y_clf_pred = clf_model.predict(X)
 
     logger.info(f'Saving predictions to {predictions_path}')
-    df_predictions=df.copy()
-    df_predictions['Predicted_Total']=y_reg_pred
-    df_predictions['Predicted_HighSpender']=y_clf_pred
+    df_predictions = df.copy()
+    df_predictions['Predicted_Total'] = y_reg_pred
+    df_predictions['Predicted_HighSpender'] = y_clf_pred
     df_predictions.to_csv(predictions_path, index=False)
 
     logger.success('Prediction complete...')
-    
 
 if __name__ == "__main__":
     app()
